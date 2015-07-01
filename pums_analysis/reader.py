cb98afc1e41865818f931def13a2197d1e424582
Dani <dbalchev@gmail.com>
2015-07-01 18:18:02 +0300

    added reader but it isnt tested

---
+++ pums_analysis/reader.py
@@ -0,0 +1,13 @@
+from csv import DictReader
+from zipfile import ZipFile
+from io import TextIOWrapper
+FILES_IN_ARCHIVE = ["ss13pus{}.csv".format(x) for x in "abcd"]
+
+def read_records_from_file(filename, record_type):
+    with ZipFile(filename) as zip_file:
+        for subfilename n FILES_IN_ARCHIVE:
+            with zip_file.open(subfilename) as bin_subfile:
+                subfile = TextIOWrapper(bin_subfile)
+                csv_reader = DictReader(subfile)
+                for record_dict in csv_reader:
+                    yield record_type(record_dict)
