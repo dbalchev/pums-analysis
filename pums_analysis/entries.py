a68840f4507b9cffe2a02ba00280a675fe78a7db
dpaskov <dpaskov91@gmail.com>
2015-07-01 18:12:01 +0300

    Updating fields init in pums_analysis. Adding fields to Entry model

--- pums_analysis/entries.py
+++ pums_analysis/entries.py
@@ -1,9 +1,12 @@
 from base.models import Model
 from base.fields import IntegerField
 
-from pums_analysis.fields import JobField
+from pums_analysis.fields import JobField, DegreeField, StateField
 
 
 class Entry(Model):
     age = IntegerField("AGEP")
     occupation = JobField("OCCP02")
+    degree = DegreeField("FOD1P")
+    state = StateField("ST")
+    wage = IntegerField("WAGP")
---
+++ pums_analysis/fields/__init__.py
@@ -0,0 +1,3 @@
+from .degrees import DegreeField
+from .occupation import JobField
+from .states import StateField
